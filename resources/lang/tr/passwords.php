bea09156ecf9fb392f59b769915aa6252536a49f
gurlab <gokhangurlab@gmail.com>
2021-02-17 05:42:48 +0300

    2. Dil ve Yapılandırma 2.1. dil ve timezone tanımı 2.2. tr dil dosyalarının eklenmesi 2.3. .env dosyasında tanımlar

---
+++ resources/lang/tr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Parolanız sıfırlandı!',
+    'sent' => 'Parola sıfırlama bağlantınızı e-posta ile gönderdik!',
+    'throttled' => 'Tekrar denemeden önce bekleyin.',
+    'token' => 'Parola sıfırlama jetonunuz hatalı.',
+    'user' => "Bu e-posta adresiyle kayıtlı kullanıcı bulunamadı.",
+
+];
