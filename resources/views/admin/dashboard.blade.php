3f57a57e5c03ba7f309f17b48c3d9d9a2152a911
gurlab <gokhangurlab@gmail.com>
2021-02-17 06:33:31 +0300

    6. breeze Dashboard & Layout Fix 6.2. UI Fix

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,7 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Anasayfa') }}
-        </h2>
-    </x-slot>
+    <x-slot name="header">Anasayfa</x-slot>
 
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
+    <div class="p-4 bg-white">
+        Giriş yaptınız!
     </div>
 </x-app-layout>
