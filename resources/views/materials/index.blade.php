76449146d5877ea79eecfebb39f384ca3c9b9697
gurlab <gokhangurlab@gmail.com>
2021-03-02 15:38:18 +0300

    main layout component

--- resources/views/materials/index.blade.php
+++ resources/views/materials/index.blade.php
@@ -1,7 +1,7 @@
-<x-app-layout>
+<x-main-layout>
     <x-slot name="header">Malzemeler Listesi</x-slot>
 
     <div class="p-4 bg-white">
         malzeme listesine hoş geldiniz.
     </div>
-</x-app-layout>
+</x-main-layout>
